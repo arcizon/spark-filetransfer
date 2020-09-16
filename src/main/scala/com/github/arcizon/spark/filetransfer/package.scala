5203ffa9e3d800cdc9bb937c45714f719009553a
Mansoor Baba Shaik <shaikmanu797@gmail.com>
2020-09-16 12:25:53 -0400

    Added implicit methods for SFTP read and write

--- src/main/scala/com/github/arcizon/spark/filetransfer/package.scala
+++ src/main/scala/com/github/arcizon/spark/filetransfer/package.scala
@@ -9,26 +9,26 @@ import org.apache.spark.sql.{DataFrame, DataFrameReader, DataFrameWriter}
 package object filetransfer {
 
   /**
-    * Adds a method, `xml`, to DataFrameReader that allows you to read avro files using
-    * the DataFileReader
+    * Adds methods for each supported [[com.github.arcizon.spark.filetransfer.util.Protocol Protocol]],
+    * to DataFrameReader that allows you to read remote files via the selected protocol.
     */
   implicit class ProtocolDataFrameReader(reader: DataFrameReader) {
     def sftp: String => DataFrame = {
       reader
-        .format("com.github.arcizon.spark")
+        .format("com.github.arcizon.spark.filetransfer")
         .option(FileTransferOptions.PROTOCOL, Protocol.sftp.toString)
         .load
     }
   }
 
   /**
-    * Adds a method, `xml`, to DataFrameWriter that allows you to write avro files using
-    * the DataFileWriter
+    * Adds methods for each supported [[com.github.arcizon.spark.filetransfer.util.Protocol Protocol]],
+    * to DataFrameWriter that allows you to write remote files via the selected protocol.
     */
   implicit class ProtocolDataFrameWriter[T](writer: DataFrameWriter[T]) {
     def sftp: String => Unit = {
       writer
-        .format("com.github.arcizon.spark")
+        .format("com.github.arcizon.spark.filetransfer")
         .option(FileTransferOptions.PROTOCOL, Protocol.sftp.toString)
         .save
     }
